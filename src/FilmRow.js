c96390291f946d8b9f4d3ca6a93abb0b8a7a8150
Leonardo Rodriguez <leo@ool-ad030072.dyn.optonline.net>
2019-02-14 16:11:49 -0500

    initial commit

---
+++ src/FilmRow.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import FilmPoster from './FilmPoster.js';
+import Fave from './Fave.js';
+
+class FilmRow extends Component {
+
+  render() {
+    return (
+      <div className="film-row" onClick={this.props.onDetailsClick}>
+        <FilmPoster film={this.props.film} />
+        <div className="film-summary">
+          <h1>{this.props.film.title}</h1>
+          <p>{new Date(this.props.film.release_date).getFullYear()}</p>
+        </div>
+
+        <Fave onFaveToggle={this.props.onFaveToggle} isFave={this.props.isFave} />
+      </div>
+    )
+  }
+}
+
+export default FilmRow
